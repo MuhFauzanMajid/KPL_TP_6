951a6f25867b4308994f7d0f2d6750613c50f4b5
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-03-24 21:58:32 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302220144/Program.cs
+++ tpmodul6_1302220144/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using tpmodul6_1302220144;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Fauzan Majid");
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(10);
+        Console.WriteLine();
+        video.PrintVideoDetails();
     }
 }
---
+++ tpmodul6_1302220144/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302220144
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string judul_video)
+        {
+            Random random = new Random();
+            
+            this.title = judul_video;
+            this.id =  random.Next(10000,99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video id : "+id);
+            Console.WriteLine("Video title : "+title);
+            Console.WriteLine("Video play count : "+playCount);
+        }
+    }
+}
